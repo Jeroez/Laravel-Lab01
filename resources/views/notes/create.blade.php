2ec548a0f1910f7592de453fc2743d33c4095be2
unknown <Prim@M305.UC-BCF.EDU.PH>
2025-02-17 08:48:26 +0800

    update

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -38,6 +38,20 @@
                     <div class="form-text text-danger">{{ $message }}</div>
                 @enderror
             </div>
+
+            <div class="mb-3">
+                <label for="inputcontent" class="form-label"><strong>content:</strong></label>
+                <textarea
+                    class="form-control @error('content') is-invalid @enderror"
+                    style="height:150px"
+                    name="subject"
+                    id="inputcontent"
+                    placeholder="content"></textarea>
+                @error('content')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
             <button type="submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Submit</button>
         </form>
 
